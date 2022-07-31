8ea2a329eeb120aafe5b5f475d561a7d5a766a70
Chris Couzens <ccouzens@gmail.com>
2022-07-31 10:44:00 +0100

    Create device in main

--- src/main.rs
+++ src/main.rs
@@ -7,63 +7,61 @@ use hidapi::HidDevice;
 const APP_ID: &str = "com.github.ccouzens.litra-glow-gtk";
 
 fn main() {
+    let api = hidapi::HidApi::new().unwrap();
+    let device = Rc::new(api.open(0x046d, 0xc900).unwrap());
+
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
     // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui());
+    app.connect_activate(move |app: &Application| build_ui(device.clone(), app));
 
     // Run the application
     app.run();
 }
 
-fn build_ui() -> impl Fn(&Application) {
-    |app: &Application| {
-        let api = hidapi::HidApi::new().unwrap();
+fn build_ui(device: Rc<HidDevice>, app: &Application) {
+    let button = ToggleButton::builder()
+        .label("Light")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
 
-        let device = Rc::new(api.open(0x046d, 0xc900).unwrap());
-        let button = ToggleButton::builder()
-            .label("Light")
-            .margin_top(12)
-            .margin_bottom(12)
-            .margin_start(12)
-            .margin_end(12)
-            .build();
+    button.connect_toggled(move |button| {
+        device
+            .write(&[
+                0x11,
+                0xff,
+                0x04,
+                0x1a,
+                if button.is_active() { 1 } else { 0 },
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+            ])
+            .unwrap();
+    });
 
-        button.connect_toggled(move |button| {
-            device
-                .write(&[
-                    0x11,
-                    0xff,
-                    0x04,
-                    0x1a,
-                    if button.is_active() { 1 } else { 0 },
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                    00,
-                ])
-                .unwrap();
-        });
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Litra Glow Control")
+        .child(&button)
+        .build();
 
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .title("Litra Glow Control")
-            .child(&button)
-            .build();
-
-        // Present window
-        window.present();
-    }
+    // Present window
+    window.present();
 }
