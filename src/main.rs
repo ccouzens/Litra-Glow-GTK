565e23de33e0545d00d7386de51015f1689f1b40
Chris Couzens <ccouzens@gmail.com>
2022-08-25 22:54:46 +0100

    Implement colour slider
    It's a lot smoother at full brightness.
    
    So far as I can tell, factors of 100 are the only values it will respond
    on

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,12 @@ fn build_ui(device: Rc<HidDevice>, app: &Application) {
     brigthness_scale.set_range(20.0, 250.0);
     brigthness_scale.set_increments(1.0, 230.0 / 5.0);
 
+    let colour_scale: Scale = builder
+        .object("colour-scale")
+        .expect("Couldn't get colour scale");
+    colour_scale.set_range(-6500.0, -2700.0);
+    colour_scale.set_increments(100.0, 800.0);
+
     button.connect_toggled(clone!(@strong device => move |button| {
         device
             .write(&[
@@ -51,7 +57,7 @@ fn build_ui(device: Rc<HidDevice>, app: &Application) {
             .unwrap();
     }));
 
-    brigthness_scale.connect_value_changed(move |scale| {
+    brigthness_scale.connect_value_changed(clone!(@strong device => move |scale| {
         device
             .write(&[
                 0x11,
@@ -60,6 +66,34 @@ fn build_ui(device: Rc<HidDevice>, app: &Application) {
                 0x4d,
                 00,
                 scale.value() as u8,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+                00,
+            ])
+            .unwrap();
+    }));
+
+    colour_scale.connect_value_changed(move |scale| {
+        let value = -scale.value() as u16;
+        device
+            .write(&[
+                0x11,
+                0xff,
+                0x04,
+                0x9d,
+                (value >> 8) as u8,
+                (value & 0x00FF) as u8,
                 00,
                 00,
                 00,
